265bb8210b80809dcb35de47c884ffef62447f39
SanderWooning <59014659+SanderWooning@users.noreply.github.com>
2020-01-14 12:46:48 +0100

    Create evaluate.py

---
+++ test/evaluate.py
@@ -0,0 +1,38 @@
+import keras
+import h5py
+import numpy as np
+import numpy as np
+from keras.layers import Input, BatchNormalization, Activation, Dense, Dropout
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
+from losses2 import *
+
+evaluate_img_generator = ImageDataGenerator()
+evaluate_img_two_generator = evaluate_img_generator.flow_from_directory(
+            "/nfs/home1/swooning/Output_Combined/test/image",
+            target_size=(512, 512),
+            color_mode="grayscale",
+            batch_size=16,
+            class_mode=None,
+            shuffle=True,
+            seed=seed)
+
+evaluate_mask_generator = ImageDataGenerator()
+evaluate_mask_two_generator = evaluate_mask_generator.flow_from_directory(
+            "/nfs/home1/swooning/Output_Combined/test/label",
+            target_size=(512, 512),
+            color_mode="grayscale",
+            batch_size=16,
+            class_mode=None,
+            shuffle=True,
+            seed=seed)
+
+        evaluate_generator = zip(evaluate_img_two_generator,
+                                 evaluate_mask_two_generator)
+
+        print('\n# Evaluate on test data')
+        results = model.evaluate_generator(evaluate_generator, steps=len(evaluate_mask_two_generator),
+                                           verbose=1)
+        print(model.metrics_names, results)
+
